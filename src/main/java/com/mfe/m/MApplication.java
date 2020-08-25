403aec54f0bef386acf910b698a571f4d39d9324
MarieDeSmedt <desmedtmarie@outlook.com>
2020-08-25 16:29:34 +0200

    initialisate spring/hibernate...

---
+++ src/main/java/com/mfe/m/MApplication.java
@@ -0,0 +1,17 @@
+package com.mfe.m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MApplication.class, args);
+
+		System.out.println("hello");
+	}
+
+
+}
